2574675ad7445da4197c9139d684b8567b0c7830
siddharth369p <sidpathak2002@gmail.com>
2026-02-11 13:54:51 +0530

    make admin pannel

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -1,18 +1,32 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const connectDB = require("./src/config/db");
 const Admin = require("./src/models/Admin");
 
+connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-.then(async () => {
-  const hashed = await bcrypt.hash("admin123", 10);
+const createAdmin = async () => {
+  try {
+    const existingAdmin = await Admin.findOne({ email: "admin@gmail.com" });
 
-  await Admin.create({
-    email: "admin@gmail.com",
-    password: hashed
-  });
+    if (existingAdmin) {
+      console.log("Admin already exists ");
+      process.exit();
+    }
 
-  console.log("Admin Created");
-  process.exit();
-});
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+
+    await Admin.create({
+      email: "admin@gmail.com",
+      password: hashedPassword,
+    });
+
+    console.log("Admin Created Successfully ");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+createAdmin();
